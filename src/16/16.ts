4ecbf05f838e4472f38a2c126f62c1be14276d30
simardo <simardo@gmail.com>
2020-11-16 21:02:43 -0500

    day 16

--- src/16/16.ts
+++ src/16/16.ts
@@ -1,11 +1,14 @@
-import { INPUT_16_Z, INPUT_16_Y, INPUT_16_X, INPUT_16_W, INPUT_16, INPUT_16_V, INPUT_16_ZZ } from "./input";
+import { INPUT_16_Z, INPUT_16_Y, INPUT_16_X, INPUT_16_W, INPUT_16, INPUT_16_V, INPUT_16_U, INPUT_16_T, INPUT_16_ZZ } from "./input";
 
 console.log('16 décembre');
 
 const base: number[] = [0, 1, 0, -1];
 
 let input1: number[] = [...INPUT_16].map(v => Number.parseInt(v));
+
 const pattern1: number[][] = Array(input1.length);
+
+console.log(input1.length);
 
 for (let i: number = 0; i < input1.length; i++) {
     pattern1[i] = [];
@@ -32,3 +35,48 @@ for (let phase: number = 1; phase <= 100; phase++) {
 }
 
 console.log(input1);
+
+console.log('part 2');
+
+let input2: number[] = [...INPUT_16].map(v => Number.parseInt(v));
+let chain: number[] = new Array(input2.length * 10000);
+
+input2.forEach((v, i) => chain[i] = v);
+
+for (let i: number = 1; i < 10000; i++) {
+    chain.copyWithin(i * input2.length, 0, input2.length);
+}
+
+const offset: number = Number.parseInt(input2.splice(0, 7).join(''));
+
+console.log(chain.length);
+console.log(offset);
+
+for (let phase: number = 1; phase <= 100; phase++) {
+    console.log('phase', phase);
+
+    const output: number[] = new Array(chain.length);
+    const outputSum: number[] = new Array(chain.length);
+
+    let c: number;
+    let n: number;
+    let first: boolean = true;
+    for (let pattern: number = offset; pattern <= chain.length; pattern++) {
+        n = 0;
+        if (first) {
+            c = pattern - 1;
+            while (c < chain.length) {
+                n += chain[c];
+                outputSum[c++] = n;
+            }
+            first = false;
+        } else {
+            n = outputSum[outputSum.length - 1] - outputSum[pattern - 2];
+        }
+
+        output[pattern - 1] = Number.parseInt(n.toString().substr(-1));
+    }
+    chain = output;
+}
+
+console.log(chain.slice(offset, offset + 8));
